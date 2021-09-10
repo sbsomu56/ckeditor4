98ed11a256d8935a2ac9258ef89570433b6c4af5
sbsomu56 <sbsomu56@gmail.com>
2021-09-10 12:09:34 +0530

    v

---
+++ config.js
@@ -0,0 +1,17 @@
+﻿/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	
+	// %REMOVE_START%
+	// The configuration options below are needed when running CKEditor from source files.
+	config.plugins = 'dialogui,dialog,about,basicstyles,blockquote,notification,button,toolbar,clipboard,resize,enterkey,entities,floatingspace,panel,floatpanel,listblock,richcombo,format,horizontalrule,wysiwygarea,indent,indentlist,fakeobjects,link,list,maximize,removeformat,sourcearea,table,undo,codeTag,imgur,eqneditor';
+	config.skin = 'moono-lisa';
+	// %REMOVE_END%
+
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+};
